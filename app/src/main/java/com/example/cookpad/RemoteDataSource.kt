8212b10514894a73d6145354f885186a913103f7
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-03-06 10:58:55 +0400

    feature-NetworkModule class added

---
+++ app/src/main/java/com/example/cookpad/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.example.cookpad
+
+class RemoteDataSource {
+}
---
+++ app/src/main/java/com/example/cookpad/data/network/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.cookpad.data.network
+
+import com.example.cookpad.FoodRecipesApi
+import com.example.cookpad.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideFoodRecipesApi(retrofit: Retrofit): FoodRecipesApi {
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL).client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory).build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS).build()
+
+    }
+}
