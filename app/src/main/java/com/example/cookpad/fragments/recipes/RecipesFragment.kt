922b79564cb1a3409f32f9ee942436c0b73b2a4b
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-03-09 13:53:09 +0400

    feature-CP-navigate to RecipesBottomSheet done

--- app/src/main/java/com/example/cookpad/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/cookpad/fragments/recipes/RecipesFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cookpad.R
 import com.example.cookpad.viewmodels.MainViewModel
 import com.example.cookpad.adapters.RecipesAdapter
 import com.example.cookpad.databinding.FragmentRecipesBinding
@@ -38,6 +40,13 @@ class RecipesFragment : Fragment() {
         setupRecyclerView()
         readDatabase()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
     }
 
     private fun setupRecyclerView() {
