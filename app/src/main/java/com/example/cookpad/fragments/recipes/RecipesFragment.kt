ff79b844d1af1d57d40a27bf3705f1a3b5611b9f
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-03-09 13:44:39 +0400

    feature-CP-RecipesBottomSheet added

--- app/src/main/java/com/example/cookpad/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/cookpad/fragments/recipes/RecipesFragment.kt
@@ -1,26 +1,21 @@
-package com.example.cookpad.fragments
+package com.example.cookpad.fragments.recipes
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.LifecycleCoroutineScope
-import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cookpad.viewmodels.MainViewModel
 import com.example.cookpad.adapters.RecipesAdapter
 import com.example.cookpad.databinding.FragmentRecipesBinding
-import com.example.cookpad.utils.Constants.API_KEY
 import com.example.cookpad.utils.NetworkResult
 import com.example.cookpad.utils.observeOnce
 import com.example.cookpad.viewmodels.RecipesViewModel
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 
 
 class RecipesFragment : Fragment() {
