335eed737e1863727f58b0b1c0db78494c5152bf
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-03-09 12:47:38 +0400

    feature-CP-Offline Caching done

--- app/src/main/java/com/example/cookpad/fragments/RecipesFragment.kt
+++ app/src/main/java/com/example/cookpad/fragments/RecipesFragment.kt
@@ -7,14 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cookpad.viewmodels.MainViewModel
 import com.example.cookpad.adapters.RecipesAdapter
 import com.example.cookpad.databinding.FragmentRecipesBinding
 import com.example.cookpad.utils.Constants.API_KEY
 import com.example.cookpad.utils.NetworkResult
+import com.example.cookpad.utils.observeOnce
 import com.example.cookpad.viewmodels.RecipesViewModel
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 
@@ -30,18 +35,42 @@ class RecipesFragment : Fragment() {
         recipesViewModel = ViewModelProvider(requireActivity())[RecipesViewModel::class.java]
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRecipesBinding.inflate(inflater, container, false)
-        Log.d("sada","222")
         setupRecyclerView()
-        requestApiData()
+        readDatabase()
         return binding.root
     }
 
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun loadDataFromCache() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner, { database ->
+                if (database.isNotEmpty()) {
+                    mAdapter.setData(database[0].foodRecipe)
+                }
+            })
+        }
+    }
+
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
+                if (database.isNotEmpty()) {
+                    mAdapter.setData(database[0].foodRecipe)
+                } else {
+                    requestApiData()
+                }
+            })
+        }
+    }
 
     private fun requestApiData() {
         mainViewModel.getRecipes(recipesViewModel.applyQueries())
@@ -51,6 +80,7 @@ class RecipesFragment : Fragment() {
                     response.data?.let { mAdapter.setData(it) }
                 }
                 is NetworkResult.Error -> {
+                    loadDataFromCache()
                     Toast.makeText(
                         requireContext(),
                         response.message.toString(),
@@ -64,12 +94,6 @@ class RecipesFragment : Fragment() {
                 ).show()
             }
         })
-    }
-
-    private fun setupRecyclerView() {
-        binding.recyclerview.adapter = mAdapter
-        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
-
     }
 
 }
